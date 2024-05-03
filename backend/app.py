94aeff1088113e1aeff1bcfaf7870ff91e8157c5
masaddee <116250759+masaddee@users.noreply.github.com>
2024-05-03 14:57:51 -0500

    Update port

--- backend/app.py
+++ backend/app.py
@@ -120,4 +120,4 @@ def disconnect(data):
 
 
 if __name__ == "__main__":
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
+    socketio.run(app, host='0.0.0.0', port=8080, debug=True, allow_unsafe_werkzeug=True)
