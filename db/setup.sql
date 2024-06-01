dd24f63e53057197065b47ecada4d6e22d378f77
trees <kongd@carleton.edu>
2024-06-01 03:40:30 -0400

    forgot this

--- db/setup.sql
+++ db/setup.sql
@@ -10,23 +10,5 @@ CREATE TABLE player (
     PRIMARY KEY(playerID)
 );
 
--- CREATE TABLE hand (
---     playerID Integer,
---     roundID Integer,
---     total Integer, -- in order to make this hand and a list, i would need to make another table hand (PK playerID roundID card)
---     bet Integer,
---     dealerTotal Integer,
--- );
-
--- CREATE TABLE round (
---     roundID Integer,
---     gameDate date, -- kinda useless
--- );
-
-INSERT INTO player VALUES (1, "kat", "kat", 1000);
-
 CREATE USER 'webapp'@'%' IDENTIFIED BY "rotmgbestgame";
 GRANT ALL ON bytejack.* TO 'webapp'@'%';
-
--- save information somewhere outside of docker and then read in each time
--- \copy history from dbsetup/course.data
